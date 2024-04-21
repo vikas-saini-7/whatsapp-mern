8206bdc8c28436ca31fc74a35740368cc6280e48
vikas-saini-7 <vikas289888@gmail.com>
2024-04-21 20:46:41 +0530

    scroll to bottom and bg image added

--- client/src/components/MessagesSection.jsx
+++ client/src/components/MessagesSection.jsx
@@ -1,41 +1,49 @@
-import React, { useEffect } from 'react'
-import LeftMessage from './LeftMessage'
-import RightMessage from './RightMessage'
-import { useDispatch, useSelector } from 'react-redux'
-import { getMessages } from '../redux/actions/messageActions'
-import { getConversation } from '../redux/actions/conversationActions'
+import React, { useEffect, useRef } from 'react';
+import LeftMessage from './LeftMessage';
+import RightMessage from './RightMessage';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMessages } from '../redux/actions/messageActions';
+import { getConversation } from '../redux/actions/conversationActions';
 
-const MessagesSection = ({flag}) => {
+const MessagesSection = ({ flag }) => {
+  const messagesContainerRef = useRef(null);
   const messages = useSelector(state => state.message.messages);
   const loading = useSelector(state => state.message.loading);
-
-  const user = useSelector(state => state.auth.user)
-  const person = useSelector(state => state.conversation.activeConversation)
+  const user = useSelector(state => state.auth.user);
+  const person = useSelector(state => state.conversation.activeConversation);
   const dispatch = useDispatch();
 
-  
   useEffect(() => {
-    person && dispatch(getConversation({senderId: user.sub, receiverId: person.sub}))
+    scrollToBottom();
+  }, [messages]);
+
+  useEffect(() => {
+    person && dispatch(getConversation({ senderId: user.sub, receiverId: person.sub }));
   }, [user, person]);
 
-  return (
-    <>
-    <div className="flex overflow-y-auto h-5/6 bg-dark mb-1" id="chats-container">
-        <div className="mx-auto w-4/5 p-0 lg:p-2 relative" id="msgContainer">
-          <span className="top-1 -ml-56 lg:ml-0 z-10 py-1.5 px-3 sticky bg-gray-700 text-sm text-gray-100 rounded-md shadow-md">TODAY</span>
-          {/* {loading && <p className='text-center text-white'>Loading..</p>} */}
-          {messages && messages.map((message) => (
-            message.receiverId === user.sub ?
-              <LeftMessage message={message.text} time={message.createdAt} />
-            :
-              <RightMessage message={message.text} time={message.createdAt}/>
-          ))}
-        </div>
-    </div>
-    </>
-    
+  const scrollToBottom = () => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTo({
+        top: messagesContainerRef.current.scrollHeight,
+        behavior: 'smooth'
+      });
+    }
+  };
 
-  )
+  return (
+    <div style={{backgroundImage:`url('images/wp-chat.jpg')`}} className="bg-contain flex overflow-y-auto h-5/6 bg-dark mb-1" id="chats-container" ref={messagesContainerRef}>
+      <div className="mx-auto w-4/5 p-0 lg:p-2 relative" id="msgContainer">
+        <span className="top-1 lg:ml-0 z-10 py-1.5 px-3 sticky bg-gray-700 text-sm text-gray-100 rounded-md shadow-md">TODAY</span>
+        {/* {loading && <p className='text-center text-white'>Loading..</p>} */}
+        {messages && messages.map((message) => (
+          message.receiverId === user.sub ?
+            <LeftMessage key={message.id} message={message.text} time={message.createdAt} />
+            :
+            <RightMessage key={message.id} message={message.text} time={message.createdAt} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default MessagesSection
+export default MessagesSection;
